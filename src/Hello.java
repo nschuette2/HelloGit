f1ce4e4d6aae202d3bedcffee58aeefd8bd25fab
noahschuette <noahschuette@noahs-mbp.unl.edu>
2025-08-28 12:38:49 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * Name: Noah Schuette
+ * 
+ * Date: August 28, 2025
+ * 
+ * A simple hello world project
+ */
+public class Hello {
+
+  public static void main(String args[]) {
+    System.out.println("Hello, I'm Noah!");
+    System.out.println("I am a Computer Engineering Major!");
+  }
+
+}
