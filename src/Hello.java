4c32ebae7fbfabe2493b8d25892157bab52df414
matthewdelgado063 <matthewdelgado063@localhost>
2025-08-28 14:19:30 -0500

    partner commit

--- src/Hello.java
+++ src/Hello.java
@@ -10,6 +10,8 @@ public class Hello {
   public static void main(String args[]) {
     System.out.println("Hello, I'm Noah!");
     System.out.println("I am a Computer Engineering Major!!");
+    System.out.println("Hello, I'm Matthew!");
+    System.out.println("I am a Computer Science Major!!");
   }
 
 }
