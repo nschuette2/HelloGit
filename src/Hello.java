b0ee347623e1f602c91068934310652756f1a329
noahschuette <noahschuette@noahs-mbp.unl.edu>
2025-08-28 13:19:11 -0500

    Changed message about major

--- src/Hello.java
+++ src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 
   public static void main(String args[]) {
     System.out.println("Hello, I'm Noah!");
-    System.out.println("I am a Computer Engineering Major!");
+    System.out.println("I am a Computer Engineering Major!!");
   }
 
 }
